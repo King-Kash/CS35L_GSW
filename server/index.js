91cd69087c372dce9771bb09f9b555f1f89b3cf4
King-Kash <kashyap.kanumuri@gmail.com>
2025-05-03 15:28:54 -0700

    5/3/2025_3:27
    Begin implementation of JWT authentication. Currently experimenting and learning. Will restructure code for better organization when possible.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,40 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
-
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 const app = express();
+connectDB();
+dotenv.config();
 
 
-app.get("/", (req,res) => {
-    res.send("Server is ready.")
+const posts = [
+    {
+        username: "Kash",
+        title: "Post 1"
+    },
+    {
+        username: "Aki",
+        title: "Post 1"
+    }
+]
+
+
+
+app.get("/posts", (req,res) => {
+    res.json(posts)
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken: accessToken}) //access token has user information saved in it
 });
 
 app.listen(3000, () => {
-    connectDB()
     console.log('Server started at http://localhost:3000');
 });
+
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const user = {name: username}
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken: accessToken}) //access token has user information saved in it
+})
