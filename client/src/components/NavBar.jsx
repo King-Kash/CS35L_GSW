257489c111cfbf97908ab643d89a4d7a6ce60875
howardshu <63481712+howardshu@users.noreply.github.com>
2025-05-07 22:03:39 -0700

    navigation bar styling + search bar component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -2,10 +2,15 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: '1rem', background: '#f5f5f5' }}>
-        <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
-      <Link to="/reviews" style={{ marginRight: '1rem' }}>Reviews</Link>
-      <Link to="/map">Map</Link>
+    <nav>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/reviews">Reviews</Link>
+        <Link to="/map">Map</Link>
+      </div>
+      <div>
+        <input type="text" placeholder="Search..." />
+      </div>
     </nav>
   );
 }
