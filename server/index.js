bf29ba027da220e09f5e96d885463efcea389c1a
Andrew Cruz <75159360+cruizeship@users.noreply.github.com>
2025-05-17 13:48:06 -0700

    Fix conflicts
    Fixed different express configs

--- server/index.js
+++ server/index.js
@@ -2,20 +2,14 @@ import express from 'express';
 import { connectDB } from './config/db.js';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import reviewRoutes from "./routers/review_router.js";
 
-import userRoutes from './controllers/users.js';
-import reviewRoutes from './controllers/reviews.js';
-import locationRoutes from './controllers/locations.js';
+import reviewRoutes from "./routers/review_router.js";
+import locationRoutes from './routers/location_router.js';
 
 const app = express();
 app.use(express.json());
 connectDB();
 dotenv.config();
-
-app.use("/users", userRoutes);
-app.use("/reviews", reviewRoutes);
-app.use("/locations", locationRoutes);
 
 const posts = [
     {
@@ -30,6 +24,7 @@ const posts = [
 
 app.use(express.json()); // Middleware to parse JSON
 app.use("/reviews", reviewRoutes); // Add review routes at /reviews
+app.use("/locations", locationRoutes); // Add location routes at /locations
 
 app.get("/posts", authenticateToken, (req,res) => {
     res.json(posts)
