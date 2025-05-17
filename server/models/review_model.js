0ec906d11e40a86081749257a3381df20cc6d23e
Andrew Cruz <75159360+cruizeship@users.noreply.github.com>
2025-05-17 10:53:37 -0700

    Create review_model.js
    Add review schema for mongoDB

---
+++ server/models/review_model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+//Template for individual review documents
+const reviewSchema = new mongoose.Schema({
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  contents: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Location',
+    required: true
+  }
+}, {
+  timestamps: true //Adds createdAt and updatedAt
+});
+
+//Review model
+const Review = mongoose.model('Review', reviewSchema); //Singular and Capital for model name.
+export default Review;
