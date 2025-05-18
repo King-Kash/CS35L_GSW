c45c4188c437ea0135f1858270709619d59f16fa
howardshu <63481712+howardshu@users.noreply.github.com>
2025-05-17 19:03:27 -0700

    navbar only on select pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 import './Home.css'; // We'll create this CSS file for styling
+import NavBar from '../components/NavBar'; // Import the NavBar component
 
 export default function Home() {
   return (
     <div className="home-container">
+      <NavBar />
       <div className="auth-buttons">
         <Link to="/login" className="auth-btn">Login</Link>
         <Link to="/register" className="auth-btn">Register</Link>
