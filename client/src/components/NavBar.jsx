72a179f8c9950fd22c3918891ca829d53caa9bed
howardshu <63481712+howardshu@users.noreply.github.com>
2025-05-31 12:45:41 -0700

    organize style files

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './NavBar.css';
+import '../styles/NavBar.css';
 import DropdownInput from './DropdownInput';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
